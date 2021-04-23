baff2b4029205bc7a5bea048f1bb99b6e4f242d5
Juan Carlos Sandoval <jcsandoval929@gmail.com>
2021-04-22 19:29:36 -0700

    Features added
    "Add" functionality created and navigation between library and wishlist implemented

--- gametrackerapp/src/app/app.component.ts
+++ gametrackerapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Game } from './game';
 import { GameService } from './game.service';
 
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
   constructor(private gameService: GameService) {}
 
   ngOnInit() {
-    this.getGames();
+    this.getLibraryGames();
   }
 
   public getGames(): void {
@@ -50,5 +51,37 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public onAddGame(addForm: NgForm): void {
+    document.getElementById('add-game-form')?.click();
+    this.gameService.addGame(addForm.value).subscribe(
+      (response: Game) => {
+        console.log(response);
+        if (response.wanted === true) {
+          this.getWishlistGames();
+        } else {
+          this.getLibraryGames();
+        }
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(game: Game | null, mode: String): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-bs-target', '#addGameModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
   }
 }
--- gametrackerapp/src/app/app.module.ts
+++ gametrackerapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +9,7 @@ import { GameService } from './game.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [GameService],
   bootstrap: [AppComponent],
 })
